23840b514216d9d61393ae1f5223f2840bc8db2b
Yvone Chan <chankeiyiu@gmail.com>
2024-01-04 18:20:44 +0900

    Finished part 5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,11 @@
+from typing import Any
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 # Create your views here.
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 
 from .models import Choice, Question
 
@@ -13,12 +15,23 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context["no_question_message"] = "No polls are available."
+        return context
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
